723227abe6991c281a03b346635a293d4dcc0ed3
Pranshu1902 <pranshu1902@gmail.com>
2022-08-02 02:03:54 +0530

    added threejs

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-red-500 text-9xl bg-gray-800 min-h-screen">
-      <h1>Home</h1>
+      <h1>The Social Dilemma</h1>
     </div>
   );
 }
