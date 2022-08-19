2b6dc2f8a77e4c14889e5948b32587165bfa3c00
Pranshu1902 <pranshu1902@gmail.com>
2022-08-20 00:15:50 +0530

    updated footer

--- src/Common/Footer.tsx
+++ src/Common/Footer.tsx
@@ -4,8 +4,18 @@ export default function Footer() {
   return (
     <div className="bg-[#FF3366] text-[#011627]">
       <div className="p-4">
-        <div className="flex">
-          <p className="text-3xl w-full">The Gladden Project</p>
+        <div className="flex justify-between">
+          <p className="text-3xl w-full flex items-center">
+            The Gladden Project
+          </p>
+          <div className="flex flex-col justify-center items-center w-1/2">
+            <div className="flex justify-center items-center hover:font-bold hover:underline">
+              <Link href="/chat">Talk</Link>
+            </div>
+            <div className=" hover:font-bold hover:underline">
+              <Link href="/about">About us</Link>
+            </div>
+          </div>
           <div className="w-full flex justify-end gap-2 text-2xl">
             <a
               href="https://twitter.com/ProjectGladden"
@@ -25,17 +35,10 @@ export default function Footer() {
             </a>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center">
-          <div className="flex justify-center items-center hover:font-bold hover:underline">
-            <Link href="/chat">Talk</Link>
-          </div>
-          <div className=" hover:font-bold hover:underline">
-            <Link href="/about">About us</Link>
-          </div>
-        </div>
       </div>
       <div className="flex justify-center items-center">
-        Made with &nbsp;<i className="fa fa-heart"></i>&nbsp; by&nbsp;
+        Made with &nbsp;<i className="fa fa-heart text-red-200"></i>&nbsp;
+        by&nbsp;
         <a
           href="https://pranshu1902.netlify.app"
           target={"_blank"}
